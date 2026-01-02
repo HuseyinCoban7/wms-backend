a109919a0d34fd3f1e4eda0f8223c4306f4cd3bc
Hüseyin Çoban <129303709+HuseyinCoban7@users.noreply.github.com>
2026-01-02 16:45:21 +0300

    Add ActiveProfiles annotation for test environment

--- src/test/java/com/wms/integration/ProductIntegrationTest.java
+++ src/test/java/com/wms/integration/ProductIntegrationTest.java
@@ -27,6 +27,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 @Testcontainers
 class ProductIntegrationTest {
 
