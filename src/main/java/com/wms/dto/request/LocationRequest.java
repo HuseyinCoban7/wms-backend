b880bda5ded7b8e713940c063f1bffb802972396
Hüseyin Çoban <129303709+HuseyinCoban7@users.noreply.github.com>
2026-01-03 20:48:20 +0300

    Add Builder annotation to LocationRequest class

--- src/main/java/com/wms/dto/request/LocationRequest.java
+++ src/main/java/com/wms/dto/request/LocationRequest.java
@@ -3,12 +3,14 @@ package com.wms.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class LocationRequest {
     @NotBlank
     private String code;
